2b2615f4e2445e689799cc8dc07cba201bb96920
mpodroid <mpodroid@gmail.com>
2023-05-24 22:55:26 +0400

    Added picture header with full name

--- qr-vcard.sh
+++ qr-vcard.sh
@@ -19,7 +19,15 @@ FULLNAME=`grep FN: ${VCARD} | cut -d':' -f2-`
 
 convert $IMAGE -quality 25% -resize 96x96 qr-codes/lowres-${INAME}
 
-sed -e "s@END:VCARD@i PHOTO;ENCODING=b;TYPE=JPEG:$(cat ${IDIR}/lowres-${INAME} | base64 )@" ${VCARD}  > qr-codes/picture-${VNAME}
-qrencode -r ${VCARD} -l M -o qr-codes/nopicture-${VNAME}.png
-qrencode -r ${VDIR}/picture-${VNAME} -l M -o qr-codes/picture-${VNAME}.png
 
+sed -e "s@^END:VCARD.*@PHOTO;ENCODING=b;TYPE=JPEG:$(cat qr-codes/lowres-${INAME} | base64 )\n&@" ${VCARD}  > qr-codes/picture-${VNAME}
+
+
+qr_encode() {
+	qrencode -r ${VCARD} -l M -o qr-codes/nopicture-${VNAME}.png	
+}
+
+qrencode -r ${VCARD} -l M -o qr-codes/nopicture-${VNAME}.png
+qrencode -r qr-codes/picture-${VNAME} -l M -o qr-codes/picture-${VNAME}.png
+
+convert qr-codes/picture-${VNAME}.png -pointsize 50 label:"${FULLNAME}" +swap -gravity Center -append qr-codes/picture-label-${VNAME}.png
